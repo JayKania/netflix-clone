caeb4f118e5487bbabca281af85d6e686d831e92
Jay Kania <jaykania123@gmail.com>
2022-02-21 22:35:49 +0530

    client side validation for login page

--- src/components/Signin/Login.jsx
+++ src/components/Signin/Login.jsx
@@ -1,29 +1,48 @@
 import { getAuth, signOut } from "firebase/auth";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { auth, firebaseApp } from "../../firebase/FirebseConfig";
 import { useAuth } from "../../context/UserContextProvider";
 import navLogo from "../../assets/Netflix-Logo.svg";
 import Footer from "../Home/Footer";
 
-const Login = ({ setUser }) => {
+const Login = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [userError, setUserError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const authObject = useAuth();
+  const userErrorRef = useRef();
+  const passwordErrorRef = useRef();
 
   const from = location.state ? location.state.form : null;
-  console.log(from);
 
   const inputHandler = (event) => {
-    event.target.id === "username"
-      ? setUserName(event.target.value)
-      : setPassword(event.target.value);
+    if (event.target.id === "username") {
+      setUserName(event.target.value);
+      setUserError("");
+    } else {
+      setPassword(event.target.value);
+      setPasswordError("");
+    }
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if (!username || !password) {
+      setShowError(false);
+      if (!username) {
+        setUserError("Username cannot be empty");
+      }
+      if (!password) {
+        setPasswordError("Password cannnot be empty");
+      }
+      return;
+    }
+
     authObject
       .login(username, password)
       .then(() => {
@@ -31,13 +50,10 @@ const Login = ({ setUser }) => {
         from ? navigate(from, { replace: true }) : navigate("/browse");
       })
       .catch((err) => {
-        console.log(err.message);
+        console.log(err.code);
+        setShowError(true);
+        setPassword("");
       });
-  };
-
-  const handleSignout = () => {
-    const auth = getAuth(firebaseApp);
-    signOut(auth);
   };
 
   return (
@@ -45,21 +61,37 @@ const Login = ({ setUser }) => {
       <img src={navLogo} alt="" className="nav-logo" />
       <div className="form-footer-container">
         <form onSubmit={submitHandler}>
-          <h2>Sign In</h2>
+          <h2>Sign In</h2>$
+          {showError ? (
+            <div className={"genric-err"}>
+              Incorrect password. Please try again or you can{" "}
+              <a href="#" id="reset-password">
+                reset your password.
+              </a>
+            </div>
+          ) : null}
           <input
-            type="text"
+            type="email"
             id="username"
             placeholder="Email or Phone Number"
             value={username}
             onChange={inputHandler}
+            className={`${userError ? "username-err" : ""}`}
           />
+          <div className="user-err" ref={userErrorRef}>
+            {userError}
+          </div>
           <input
             type="password"
             id="password"
             placeholder="Password"
             value={password}
             onChange={inputHandler}
+            className={`${passwordError ? "password-err" : ""}`}
           />
+          <div className="pass-err" ref={passwordErrorRef}>
+            {passwordError}
+          </div>
           <button type="submit">Sign in</button>
           <div className="checkbox-help-wrapper">
             <div className="checkbox">
